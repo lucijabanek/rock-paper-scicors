65b7ba9568b73277d273ae7cccd2321baaa6913d
lucijabanek <lbanek00@fesb.hr>
2022-11-13 01:17:20 +0100

    Adding points counter and results of round to be displayed on page

--- script.js
+++ script.js
@@ -1,102 +1,72 @@
-const choice = ["rock", "paper", "scissors"]
-let playerSelection = null;
-let computerSelection = 0;
 let playerScore = 0;
 let computerScore = 0;
+const roundWinner = document.getElementById("Winner");
 
 let getPlayerChoice = (playerSelection) => {
 
-    let rock = document.getElementById("imageRock");
-    let paper = document.getElementById("imagePaper");
-    let scissors = document.getElementById("imageScissors");
-
-    // console.log(playerChoice);
-
-    if (playerSelection == rock) {
-        playerSelection = "rock";
-    } else if (playerSelection == paper) {
-        playerSelection = "paper";
-    } else if (playerSelection == scissors) {
-        playerSelection = "scissors";
-    } else {
-             getPlayerChoice();
-    }
+    const choice = ["rock", "paper", "scissors"]
+    let computerSelection = 0;
+    let playerChoice = playerSelection.id;
 
     computerSelection = getComputerChoice(choice);
-    console.log(playerSelection);
-    console.log(computerSelection);
-    playRound(playerSelection, computerSelection);
+    playRound(playerChoice, computerSelection);
 }
 
 let getComputerChoice = (choice) => {
-    let randomChoice = choice[Math.floor(Math.random()*choice.length)];
+    let randomChoice = choice[Math.floor(Math.random() * choice.length)];
     return randomChoice;
 }
 
+let playRound = (playerSelection, computerSelection) => {
 
-// function playRound(playerSelection, computerSelection) {
-//     return
-//     (computerSelection === "rock" && playerSelection === "paper") ? playerWin()
-//     : (computerSelection === "rock" && playerSelection === "scissors") ? computerWin()
-//     : (computerSelection === "paper" && playerSelection === "rock") ? computerWin()
-//     : (computerSelection === "paper" && playerSelection === "scissors") ? playerWin()
-//     : (computerSelection === "scissors" && playerSelection === "paper") ? computerWin()
-//     : (computerSelection === "scissors" && playerSelection === "rock") ? playerWin()
-//     : "It's tie!";
-// }
-
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection === "rock" && playerSelection === "paper") {
-        console.log("You beat computer: paper beats rock!")
-        playerScore++;
-        console.log(playerScore, computerScore);
-    } 
-    else if (computerSelection === "rock" && playerSelection === "scissors") {
-        console.log("Computer beats you: rock beats scissors!")
-        computerScore++;
-        console.log(playerScore, computerScore);
+    if (playerSelection === computerSelection) {
+        roundWinner.textContent = `${playerSelection} vs. ${computerSelection}: It's tie!`;
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            playerScore++;
+            roundWinner.textContent = `paper vs. rock: Player beat computer!`;
+        } else {
+            computerScore++;
+            roundWinner.textContent = `paper vs scissors: Computer beats player!`;
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            playerScore++;
+            roundWinner.textContent = `scissors vs. paper: Player beat computer!`;
+        } else {
+            computerScore++;
+            roundWinner.textContent = `scissors vs. rock: Computer beats player!`;
+        }
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            playerScore++;
+            roundWinner.textContent = `rock vs. scissors: Player beat computer!`;
+        }
+        else {
+            computerScore++;
+            roundWinner.textContent = `rock vs. paper: Computer beats player!`;
+        }
     }
-    else if (computerSelection === "paper" && playerSelection === "rock") {
-        console.log("Computer beats you: paper beats rock!")
-        computerScore++;
-        console.log(playerScore, computerScore);
-    }
-    else if (computerSelection === "paper" && playerSelection === "scissors") {
-        console.log("You beat computer: scissors beats paper!")
-        playerScore++;
-        console.log(playerScore, computerScore);
-    }
-    else if (computerSelection === "scissors" && playerSelection === "paper") {
-        console.log("Computer beats you: scissors beats paper!")
-        computerScore++;
-        console.log(playerScore, computerScore);
-    }
-    else if (computerSelection === "scissors" && playerSelection === "rock") {
-        console.log("You beat computer: rock beats scissors!")
-        playerScore++;
-        console.log(playerScore, computerScore);
-    }
-    else {
-        console.log ("It's tie!");
-        console.log(playerScore, computerScore);
-    }
+    updateScore();
+    gameWinner();
 }
 
-// let game = () => {
-//     while (playerScore < 5 && computerScore < 5) {
-//         getPlayerChoice(playerChoice);
-//         console.log(playerSelection);
-//         computerSelection = getComputerChoice(choice);
-//         console.log(computerSelection);
-//         playRound(playerSelection, computerSelection);
+function updateScore() {
+    const playerCounterPara = document.getElementById("playerCounter");
+    const computerCounterPara = document.getElementById("computerCounter");
 
-//         if(playerScore == 5) {
-//             console.log("Player win!");
-//         } else if (computerScore == 5){
-//             console.log("Computer win!")
-//         } else 
-//         continue;
-//     }
-// }   
+    playerCounterPara.textContent = `${playerScore}`;
+    computerCounterPara.textContent = `${computerScore}`;
+}
 
-// game();
+function gameWinner() {
+    if (playerScore === 5) {
+        playerScore = 0;
+        computerScore = 0;
+        roundWinner.textContent = `Player win!`;
+    } else if (computerScore === 5) {
+        playerScore = 0;
+        computerScore = 0;
+        roundWinner.textContent = `Computer win!`;
+    }
+}
